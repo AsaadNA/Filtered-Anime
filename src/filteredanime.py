13e61d6d71693041aa57096184513db97d7acb21
Asaad Abbasi <asaad.abbasi@gmail.com>
2021-02-07 20:34:18 +0500

    quality option , automatic quality control , args and it's error checking , folder creation

--- src/filteredanime.py
+++ src/filteredanime.py
@@ -8,13 +8,21 @@ from urllib.parse import parse_qs
 from threading import Thread
 from tqdm import tqdm
 
+#Get episode count
+#http Error checking
+
+#argument error checking
+#quality option
+#automatic priority wise quality control
+#folder creation
+
 class progessBar(tqdm):
     def update_to(self, b=1, bsize=1, tsize=None):
         if tsize is not None:
             self.total = tsize
         self.update(b * bsize - self.n)
 
-def downloadVideo(animeName,episode):
+def downloadVideo(animeName,episode,quality):
 
    result_name = []
    result_links = []
@@ -47,25 +55,152 @@ def downloadVideo(animeName,episode):
       result_name.append(name)
       result_links.append(downloadLink)
 
-   fileName = animeName + " "  + str(episode) + ".mp4"
-   with progessBar(unit='B', unit_scale=True,
-                             miniters=1, desc=url.split('/')[-1]) as t:
-      urllib.request.urlretrieve(result_links[0],fileName,reporthook=t.update_to) 
+   fileName = animeName + " "  + str(episode)  
+   fullFileName = ' '
+   idx = -1
+
+   #Sorting out the Quality Control if one quality is not found
+   #Download the quality as to priority
+   if quality == 'l':
+
+      #if 360p download
+      if '\n(360P-mp4)' in result_name:
+         idx = result_name.index('\n(360P-mp4)')
+         fileName += " [360P].mp4"
+         fullFileName = os.path.join(animeName,fileName)
+
+      #if not download other res
+      else:
+
+         #first priority to 480P
+         if '\n(480P-mp4)' in result_name:
+            idx = result_name.index('\n(480P-mp4)')
+            fileName += " [480P].mp4"
+            fullFileName = os.path.join(animeName,fileName)
+
+         #second priority
+         elif '\n(720P-mp4)' in result_name:
+            idx = result_name.index('\n(720P-mp4)')
+            fileName += " [720P].mp4"
+            fullFileName = os.path.join(animeName,fileName)
+            
+         else:
+            print('no quality avail')
+
+   #MEDIUM 480P
+   elif quality == 'm':
+ 
+      #if 480p then download
+      if '\n(480P-mp4)' in result_name:
+         idx = result_name.index('\n(480P-mp4)')
+         fileName += " [480P].mp4"
+         fullFileName = os.path.join(animeName,fileName)
+
+      #if not download other res
+      else:
+
+         #first priority to 720p
+         if '\n(720P-mp4)' in result_name:
+            idx = result_name.index('\n(720P-mp4)')
+            fileName += " [720P].mp4"
+            fullFileName = os.path.join(animeName,fileName)
+
+         #second priority
+         elif '\n(360P-mp4)' in result_name:
+            idx = result_name.index('\n(360P-mp4)')
+            fileName += " [360P].mp4"
+            fullFileName = os.path.join(animeName,fileName)
+            
+         else:
+            print('no quality avail')
+
+   #HIGH 720P
+   else:
+
+      #if 720P then download
+      if '\n(720P-mp4)' in result_name:
+         idx = result_name.index('\n(720P-mp4)')
+         fileName += " [720P].mp4"
+         fullFileName = os.path.join(animeName,fileName)
+
+      #if not download other res
+      else:
+
+         #first priority to 480P
+         if '\n(480P-mp4)' in result_name:
+            idx = result_name.index('\n(480P-mp4)')
+            fileName += " [480P].mp4"
+            fullFileName = os.path.join(animeName,fileName)
+
+         #second priority
+         elif '\n(360P-mp4)' in result_name:
+            idx = result_name.index('\n(360P-mp4)')
+            fileName += " [360P].mp4"
+            fullFileName = os.path.join(animeName,fileName)
+            
+         else:
+            print('no quality avail')
+   
+   #download
+   with progessBar(unit="B",unit_scale=True,miniters=1,desc=url.split('/')[-1]) as t:
+            urllib.request.urlretrieve(result_links[idx],fullFileName,reporthook=t.update_to)   
+
+def isThere(animeName,quality,idx,fileName,fullFileName,resultName):
+   if quality in resultName:
+      idx = resultName.index(quality)
+      quality = quality.replace('\n','')
+      fileName += " " + quality + ".mp4"
+      fullFileName = os.path.join(animeName,fileName)
+      #print(fullFileName)
+      return True
+   return False
 
 #@NOTE: animeName should contain - instead of space
 
 #animeName -> name of anime you wanna download
 #start -> starting episode
 #stop  -> ending episode
-def startDownload(animeName,start,stop):
+def multiDownload(animeName,start,stop,quality):
    all_threads = []
    _queue = queue.Queue()
    if start == stop:
       downloadVideo(animeName,start)
    else:
       for i in range(start,stop+1):
-         t = Thread(target=downloadVideo,args=(animeName,i))
+         t = Thread(target=downloadVideo,args=(animeName,i,quality))
          t.start()
          all_threads.append(t)
 
-startDownload('gintama',252,252)
+
+#python filteredanime [anime] [startEpisode] [stopEpisode] [quality] //BULK
+#python filteredanime [anime] [episode] [quality] //SINGLE EPISODE
+
+#quality modes 
+
+  # l => low => 360p
+  # m => medium => 480p
+  # h => high => 720p
+
+#Example : python filteredanime gintama 245 250 h => will download anime gintama from episode 245 to 250 in HIGH [720P] 
+
+#Save the downloaded files in their respective folders
+def makeFolderIfNotCreate(animeName):
+   if os.path.exists(animeName) == False:
+      os.mkdir(animeName)
+
+if __name__ == '__main__':
+   argLength = len(sys.argv)-1
+   if argLength == 4:
+      if ((sys.argv[4] != 'l') and (sys.argv[4] != 'm') and (sys.argv[4] != 'h')) or (sys.argv[2].isdigit() == False) or (sys.argv[3].isdigit() == False) or (int(sys.argv[2]) > int(sys.argv[3])) or (int(sys.argv[2]) == int(sys.argv[3])):
+         print('invalid arguments: check params')
+      else:
+         makeFolderIfNotCreate(sys.argv[1])
+         multiDownload(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]),sys.argv[4])
+   elif argLength == 3:
+      if ((sys.argv[3] != 'l') and (sys.argv[3] != 'm') and (sys.argv[3] != 'h')) or (sys.argv[2].isdigit() == False):
+         print('invalid arguments: check params')
+      else:
+         makeFolderIfNotCreate(sys.argv[1])
+         downloadVideo(sys.argv[1],sys.argv[2],sys.argv[3])
+   else:
+      print('invalid arguments: check params')
